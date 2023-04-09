bc6c488293aaaaab48aa68cdc8d3f2ec1486b331
sevdeozen <svdozen12@gmail.com>
2023-04-10 00:00:31 +0300

    added cart

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const categoryList = document.querySelector(".category-list");
+const productList = document.querySelector(".product-list");
+const openBtn = document.getElementById("cart-btn");
+const closeBtn = document.querySelector("#close-btn");
+const modal = document.getElementById("modal-wrapper");
 document.addEventListener("DOMContentLoaded", () => {
   fetchCategories();
+  fetchProducts();
 });
 
 const fetchCategories = () => {
@@ -17,3 +22,53 @@ const fetchCategories = () => {
     )
     .catch((err) => console.log(err));
 };
+
+const fetchProducts = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) =>
+      data.slice(3, 20).forEach((product) => {
+        const productDiv = document.createElement("div");
+        productDiv.classList.add("card");
+        productDiv.innerHTML = `  <img src="${product.image}" >
+        <p>${product.title}</p>
+        <p>${product.category}</p>
+        <div class="card-footer">
+            <span>${product.price} $</span>
+            <button onClick='addCart({id:"${product.id}", name:"${product.title}",price:"${product.price}", img:"${product.image}",amount:1 })' >Add a Cart</button>
+        </div>`;
+        productList.appendChild(productDiv);
+      })
+    );
+  // .catch((err) => console.log(err));
+};
+//add cart
+
+const cart = [];
+
+function listCart() {
+  cart.forEach((item) => {
+    console.log(item);
+  });
+}
+
+/*modal */
+const toggleModal = () => {
+  modal.classList.toggle("active");
+};
+
+openBtn.addEventListener("click", () => {
+  toggleModal();
+  listCart();
+});
+closeBtn.addEventListener("click", toggleModal);
+
+function addCart(param) {
+  const foundItem = cart.find((eleman) => eleman.id === param.id);
+
+  if (foundItem) {
+    foundItem.amount += 1;
+  } else {
+    cart.push(param);
+  }
+}
